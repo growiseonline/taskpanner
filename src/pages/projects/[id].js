2006a470271979b4828a0d0734027960d7cf67b7
augustogrowise <augusto.mmorais@outlook.com>
2023-02-10 12:22:38 -0300

    second commint

--- src/pages/projects/[id].js
+++ src/pages/projects/[id].js
@@ -39,19 +39,6 @@ Page.getLayout = (page) => (
 );
 
 
-export async function getStaticPaths() {
-  return {
-    paths: [{ params: { id: '1' } }, { params: { id: '2' } }],
-    fallback: false, // can also be true or 'blocking'
-  }
-}
 
-// `getStaticPaths` requires using `getStaticProps`
-export async function getStaticProps(context) {
-  return {
-    // Passed to the page component as props
-    props: { post: {} },
-  }
-}
 
 export default Page;
